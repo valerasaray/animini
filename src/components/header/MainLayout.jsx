d2b7ff2a8dd5144037f76130de14d13cf2b0fdc2
crewch <artemkr2003@mail.ru>
2023-02-07 00:06:26 +0300

    +

--- src/components/header/MainLayout.jsx
+++ src/components/header/MainLayout.jsx
@@ -1,18 +1,22 @@
 import { Link, Outlet } from 'react-router-dom';
+import { FiMoon } from 'react-icons/fi';
 
 function MainLayout() {
     return (
         <>
-            <header className="p-2 h-16 w-full shadow-gray-300 shadow-sm bg-white absolute">
-                <nav className="bg-white">
+            <header className="pl-2 pr-2 h-6 sm:h-16 w-full shadow-gray-300 shadow-sm bg-white absolute">
+                <nav className="bg-white flex justify-between">
                     <button>
                         <Link to="/" className="w-min">
                             <img
                                 src="logo.png"
                                 alt="logo"
-                                className="h-12 bg-white"
+                                className="h-6 sm:h-16 bg-white"
                             />
                         </Link>
+                    </button>
+                    <button>
+                        <FiMoon className="h-6 w-6 sm:h-16 sm:w-16 bg-transparent" />
                     </button>
                 </nav>
             </header>
