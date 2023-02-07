c032b874fe0c98c43bcd3ddf02be6e07ae5bcb1e
crewch <artemkr2003@mail.ru>
2023-02-07 09:46:08 +0300

    fix

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,14 +5,13 @@ function MainPage() {
     return (
         <main className="h-full">
             <div className="h-1/5" />
-            <section className="h-3/5">
+            <section>
                 <MainPageImages />
                 <h1 className="flex flex-wrap justify-center font-bold p-3 text-sm sm:text-xl text-center">
                     Генерация минималистичных аниме-артов
                 </h1>
                 <MainPageButtons />
             </section>
-            <div className="h-1/5" />
         </main>
     );
 }
