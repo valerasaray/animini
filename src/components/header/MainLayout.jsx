54190d5000356141b21dc89a1e9346caa6221116
crewch <artemkr2003@mail.ru>
2023-02-07 10:11:34 +0300

    )))

--- src/components/header/MainLayout.jsx
+++ src/components/header/MainLayout.jsx
@@ -4,8 +4,8 @@ import { FiMoon, FiSun } from 'react-icons/fi';
 function MainLayout() {
     return (
         <>
-            <header className="pl-2 pr-2 h-6 sm:h-16 w-full shadow-gray-300 shadow-sm bg-white absolute">
-                <nav className="bg-white flex justify-between">
+            <header className="h-6 sm:h-16 w-full shadow-gray-300 shadow-sm bg-white absolute">
+                <nav className="pl-2 bg-white flex justify-between">
                     <button>
                         <Link to="/" className="w-min">
                             <img
@@ -15,7 +15,7 @@ function MainLayout() {
                             />
                         </Link>
                     </button>
-                    <button>
+                    <button className='mr-2'>
                         <FiMoon className="h-6 w-6 sm:h-16 sm:w-16 bg-transparent" />
                         {/* <FiSun className="h-5 w-5 sm:h-14 sm:w-14 bg-transparent" /> */}
                     </button>
